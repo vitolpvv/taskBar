e2ea4b52428ceeced027e19f8e5efe8323befd58
NikolDemi <n.demicheva@gmail.com>
2019-10-11 12:28:57 +0300

    Changed index for main

--- src/main/java/com/geekbrains/controllers/MainPageController.java
+++ src/main/java/com/geekbrains/controllers/MainPageController.java
@@ -1,4 +1,19 @@
 package com.geekbrains.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainPageController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "main";
+    }
+
+    @GetMapping("/mainPage")
+    public String MainPage() {
+        return "mainPage";
+    }
 }
