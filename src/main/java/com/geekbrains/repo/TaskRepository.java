21c257880ea6f6618d789c90a4a19b72cd8794a0
Vitaly Parshin <vitol_pvv@mail.ru>
2019-10-11 08:49:06 +0300

    Java-doc deleted.

--- src/main/java/com/geekbrains/repo/TaskRepository.java
+++ src/main/java/com/geekbrains/repo/TaskRepository.java
@@ -10,19 +10,7 @@ import java.util.List;
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
-    /**
-     * Find all tasks by username.
-     *
-     * @param username - username (@see java.lang.String)
-     * @return list of tasks (@see com.geekbrains.Task)
-     */
     List<Task> findAllByUsername(String username);
 
-    /**
-     * Find all tasks by target date.
-     *
-     * @param date - date (@see java.time.LocalDate)
-     * @return list of tasks (@see com.geekbrains.Task)
-     */
     List<Task> findAllByTargetDate(LocalDate date);
 }
