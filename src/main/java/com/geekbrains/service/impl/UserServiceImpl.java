ca643a0967f51ed6bbf4c18d469b2545e229b490
Vitaly Parshin <vitol_pvv@mail.ru>
2019-10-15 21:42:41 +0300

    User service interface and implementation added.

---
+++ src/main/java/com/geekbrains/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.geekbrains.service.impl;
+
+import com.geekbrains.domain.User;
+import com.geekbrains.repo.UserRepository;
+import com.geekbrains.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl extends ServiceImpl<User, Long> implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    CrudRepository<User, Long> getRepository() {
+        return repository;
+    }
+
+    @Override
+    public User getByUsername(String name) {
+        return repository.findByUsername(name);
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return repository.findByEmail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = getByUsername(s);
+        if (user == null) {
+            user = getByEmail(s);
+        }
+        if (user == null) {
+            throw new UsernameNotFoundException(s);
+        }
+        return user;
+    }
+}
