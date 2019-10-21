743ac521640053f5fbdde47893ad80f531d54aee
Stas Kolenchik <stas.kolenchik@gmail.com>
2019-10-21 12:18:01 +0300

    add access to css resources

--- src/main/java/com/geekbrains/config/WebSecurityConfig.java
+++ src/main/java/com/geekbrains/config/WebSecurityConfig.java
@@ -18,11 +18,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/css/**").permitAll()
                     .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
+                .and()
+                    .formLogin()
+                        .loginPage("/login").permitAll()
                 .and()
                     .logout()
                     .permitAll();
